ce76fbd9078d3e3b1fcc3d4631c17c8741aaf1c5
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-05-28 21:58:38 +0300

    add html

---
+++ content.js
@@ -0,0 +1,10 @@
+var $page = $("html, body");
+$('a[href*="#"]').click(function () {
+  $page.animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    1500
+  );
+  return false;
+});
